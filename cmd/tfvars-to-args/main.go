d5ccbf0f151ca2764cd2a7f8854cc145cb768c8c
Ian Douglas <iancaseydouglas@gmail.com>
2024-08-14 12:43:50 -0600

    change default ordering or cli flags

--- cmd/tfvars-to-args/main.go
+++ cmd/tfvars-to-args/main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
+	reverse := flag.Bool("reverse", false, "Reverse the order of var-file arguments")
+	flag.Parse()
+
+	var files []string
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		fmt.Printf("-var-file=%s ", scanner.Text())
+		files = append(files, scanner.Text())
+	}
+
+	if !*reverse {
+		// Reverse the slice
+		for i := len(files)/2 - 1; i >= 0; i-- {
+			opp := len(files) - 1 - i
+			files[i], files[opp] = files[opp], files[i]
+		}
+	}
+
+	for _, file := range files {
+		fmt.Printf("-var-file=%s ", file)
 	}
 	fmt.Println()
 }
